3a45e65262f582c0b269d828b8bca36d44889c52
paul-hess <pauljacksonhess@gmail.com>
2016-05-02 21:17:25 -0700

    add database relation to tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,65 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
 public class Task {
-  private String mDescription;
+  private String description;
+  private boolean completed;
+  private LocalDateTime createdAt;
+  private int id;
 
   public Task (String description) {
-    mDescription = description;
+    this.description = description;
+    this.completed = false;
+    this.createdAt = LocalDateTime.now();
+  }
+
+  @Override
+  public boolean equals(Object otherTask) {
+    if(!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription()) && 
+      this.getId() == newTask.getId();
+    }
+  }
+
+
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO tasks (description) VALUES (:description)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("description", this.description)
+        .executeUpdate()
+        .getKey();
+    }
   }
 
   public String getDescription() {
-    return mDescription;
+    return description;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public boolean isCompleted() {
+  	return completed;
+  }
+
+  public LocalDateTime getCreatedAt() {
+  	return createdAt;
+  }
+
+  public static List<Task> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT id, description FROM tasks";
+     return con.createQuery(sql).executeAndFetch(Task.class);
+
+    }
+
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,17 +1,86 @@
 import org.junit.*;
+import org.sql2o.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
+  @Before 
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After 
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM tasks *";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
   @Test
   public void Task_instantiatesCorrectly_true() {
-    Task myTask = new Task("Do the dishes");
-    assertEquals(true, myTask instanceof Task);
+    Task testTask = new Task("Do the dishes");
+    assertEquals(true, testTask instanceof Task);
   }
 
   @Test
   public void Task_instantiatesWithDescription_String() {
-    Task myTask = new Task("Mow the lawn");
-    assertEquals("Mow the lawn", myTask.getDescription());
+    Task testTask= new Task("Mow the lawn");
+    assertEquals("Mow the lawn", testTask.getDescription());
   }
+
+  @Test 
+  public void all_InstantiatesEmpty() {
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Trim the hedge");
+    Task secondTask = new Task("Trim the hedge");
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Task testTask = new Task("Do the laundry");
+    testTask.save();
+    assertTrue(Task.all().get(0).equals(testTask));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Task testTask = new Task("Mow the lawn");
+    testTask.save();
+    Task savedTask = Task.all().get(0);
+    assertEquals(testTask.getId(), savedTask.getId());
+}
+
+  // @Test 
+  // public void Task_isCompleted_isFalseAfterInstantiation_false() {
+  //   Task testTask= new Task("Mow the lawn");
+  //   assertEquals(false, myTask.isCompleted());
+  // }
+
+  // @Test 
+  // public void getCreatedAt_instantiatesWithCurrentTime_today() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  // }
+
+  // @Test 
+  // public void all_returnsAllInstancesOfTask_true() {
+  //   Task firstTask = new Task("Mow the lawn");
+  //   Task secondTask = new Task("Buy groceries");
+  //   assertTrue(Task.all().contains(firstTask));
+  //   assertTrue(Task.all().contains(secondTask));
+  // }
+
+  // @Test
+  // public void clear_emptiedAllTasksFromArrayList_0() {
+  //   Task myTask = new Task("Mow the lawn");
+  //   Task.clear();
+  //   assertEquals(Task.all().size(), 0);
+  // }
 }
